617e1c0eb74e90945ba2e806e1592b1942df42da
zjaffrey <114377448+zjaffrey@users.noreply.github.com>
2024-05-16 13:44:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+/**
+ * This function adds an element to the Unordered List
+ * When an element is added to the list it should have an onclick that allows you to remove the same element
+ */
+function addItem() {
+    // This code is the Unique ID for the Button, do not change this
+    const uniqueId = Math.random().toString(36).substr(2, 9);
 
+}
+
+/**
+ * This function will be used to remove the item that was added in the unordered list
+ */
+function removeItem(itemId) {
+
+}
